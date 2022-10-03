6a344ebfaee10339f8e334571f0ed7a73b63e69b
Charlie0107 <61020659+Charlie0107@users.noreply.github.com>
2022-10-03 00:18:01 -0500

    Update PrivateSide.js

--- asap/src/components/PrivateSide.js
+++ asap/src/components/PrivateSide.js
@@ -400,6 +400,29 @@ const PrivateSide = () => {
               <span className="hide-menu">Administradores</span>
             </a>
           </li>
+              
+          <li className="sidebar-item">
+              {" "}
+              <a className="sidebar-link sidebar-link" href="/account">
+              <svg xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                stroke-width="2" 
+                stroke-linecap="round" 
+                stroke-linejoin="round" 
+                class="feather feather-user">
+                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+                <rect x={3} y={3} width={18} height={18} rx={2} ry={2} />
+                <line x1={9} y1={3} x2={9} y2={21} />
+                
+                <span className="hide-menu">Perfil</span>
+              </a>
+          </li>
 
         
 
