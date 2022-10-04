ccd10061edf2dbb973fbc9daf4051cfa412427ef
Guillermo Leyva Castañón <memoleycast@gmail.com>
2022-10-04 04:03:42 -0500

    Login

--- asap/src/pages/Login.js
+++ asap/src/pages/Login.js
@@ -2,81 +2,60 @@ import React from "react";
 
 export default function Login() {
   return (
-    <>
-      <div className="main-wrapper">
-        {/* ============================================================== */}
-        {/* Preloader - style you can find in spinners.css */}
-        {/* ============================================================== */}
-        {/* <div className="preloader">
-          <div className="lds-ripple">
-            <div className="lds-pos" />
-            <div className="lds-pos" />
-          </div>
-        </div> */}
-        {/* ============================================================== */}
-        {/* Preloader - style you can find in spinners.css */}
-        {/* ============================================================== */}
-        {/* ============================================================== */}
-        {/* Login box.scss */}
-        {/* ============================================================== */}
+  <div className="main-wrapper">
         <div
           className="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
           style={{
-            background: "url(assets/images/img1.jpg) no-repeat center center",
+            background: "url(assets/images/big/auth-bg2.jpg) no-repeat center center",
+            backgroundSize: "cover",
           }}
         >
           <div className="auth-box row">
             <div
               className="col-lg-7 col-md-5 modal-bg-img"
-              style={{ backgroundImage: "url(assets/images/img1.jpg)" }}
+              style={{ backgroundImage: "url(assets/images/users/1.jpg)" }}
             ></div>
             <div className="col-lg-5 col-md-7 bg-white">
               <div className="p-3">
                 <div className="text-center">
-                  <img src="assets/images/big/icon.png" alt="wrapkit" />
+                  <img src="assets/images/log-in.png" alt="wrapkit" />
                 </div>
-                <h2 className="mt-3 text-center">Sign In</h2>
+                <h2 className="mt-3 text-center">Inicio de sesión</h2>
                 <p className="text-center">
-                  Enter your email address and password to access admin panel.
+                Ingrese su dirección de correo electrónico y contraseña para acceder al panel de administración.
                 </p>
                 <form className="mt-4">
                   <div className="row">
                     <div className="col-lg-12">
                       <div className="form-group">
                         <label className="text-dark" htmlFor="uname">
-                          Username
+                          Correo electrónico
                         </label>
                         <input
                           className="form-control"
-                          id="uname"
+                          id="email"
                           type="text"
-                          placeholder="enter your username"
+                          placeholder="ingrese su correo electrónico"
                         />
                       </div>
                     </div>
                     <div className="col-lg-12">
                       <div className="form-group">
                         <label className="text-dark" htmlFor="pwd">
-                          Password
+                          Contraseña
                         </label>
                         <input
                           className="form-control"
-                          id="pwd"
+                          id="password"
                           type="password"
-                          placeholder="enter your password"
+                          placeholder="ingrese su contraseña"
                         />
                       </div>
                     </div>
                     <div className="col-lg-12 text-center">
                       <button type="submit" className="btn btn-block btn-dark">
-                        Sign In
+                      Iniciar sesión
                       </button>
-                    </div>
-                    <div className="col-lg-12 text-center mt-5">
-                      Don't have an account?{" "}
-                      <a href="#" className="text-danger">
-                        Sign Up
-                      </a>
                     </div>
                   </div>
                 </form>
@@ -84,10 +63,6 @@ export default function Login() {
             </div>
           </div>
         </div>
-        {/* ============================================================== */}
-        {/* Login box.scss */}
-        {/* ============================================================== */}
       </div>
-    </>
   );
 }
